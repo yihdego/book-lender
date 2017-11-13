3415fc2b877baa732bbd91ffdad790f2c2f5df78
Amanuel <yihdego@msn.com>
2017-11-13 12:01:24 -0800

    password confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let!(:user) { User.create(username: 'yihdego', email: 'amanuel@email.com', password: 'password') }
+  let!(:user2) { User.new(username: 'bob', email: 'bob@email.com') }
   describe 'attributes' do
     it 'should have a username' do
       expect(user.username).to eq 'yihdego'
@@ -10,8 +11,21 @@ RSpec.describe User, type: :model do
       expect(user.email).to eq 'amanuel@email.com'
     end
     it 'should have a password' do
-      p user.password
       expect(user.password).to be_a String
+    end
+
+    it 'should not be valid if the confirmation password does not match' do
+      user2.password = 'marley'
+      user2.password_confirmation = 'legend'
+      user2.save
+      expect(user2).to_not be_valid
+    end
+
+    it 'should be valid if they match' do
+      user2.password = 'marley'
+      user2.password_confirmation = 'marley'
+      user2.save
+      expect(user2).to be_valid
     end
   end
 end
