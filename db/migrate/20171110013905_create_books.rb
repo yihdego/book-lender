4ee866de88334ee5ce2c6aa3598e51fc2ceecd66
Amanuel <yihdego@msn.com>
2017-11-15 12:46:55 -0800

    changed book lender books to only care about title and author

--- db/migrate/20171110013905_create_books.rb
+++ db/migrate/20171110013905_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :isbn
+      t.string :author
 
       t.timestamps
     end
--- spec/models/shelf_spec.rb
+++ spec/models/shelf_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Shelf, type: :model do
   let!(:user) { User.create(username: 'yihdego', email: 'amanuel@email.com', password: 'password') }
   let!(:user2) { User.new(username: 'bob', email: 'bob@email.com') }
-  let!(:book) { {title: "Javascript & JQuery", isbn: "1234567890"} }
+  let!(:book) { {title: "Javascript & JQuery", author: "Jon Duckett"} }
   describe 'adding a book to a users shelf' do
     it 'can add a book to a users collection from the api' do
       user.add_book(book)
